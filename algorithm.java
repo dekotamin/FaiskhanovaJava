2b23c2ea30265d13d3f1e21643247797c8753e10
Alina Faiskhanova <nyrete@gmail.com>
2022-05-16 00:16:05 +0500

    Добавлен .gitignore

---
+++ algorithm.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class algoritm {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+        if (num > 7) {
+            System.out.printf("Привет!");
+            in.close();
+        }
+    }
+}
